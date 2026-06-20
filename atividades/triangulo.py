e1131009a5712ccbd9c4ee8f137b90a7cce4837f
João Vitor Rodrigues <135203823+Burakku-Jon@users.noreply.github.com>
2026-06-19 23:54:39 -0300

    Fix triangle inequality condition
    Change triangle inequality check from 'or' to 'and'.

--- atividades/triangulo.py
+++ atividades/triangulo.py
@@ -2,7 +2,7 @@ l1 = int(input('digte um numero: '))
 l2 = int(input('digte um numero: '))
 l3 = int(input('digte um numero: '))
 
-if l1 + l2 > l3 or l1 + l3 > l2 or l3 + l2 > l1:
+if l1 + l2 > l3 and l1 + l3 > l2 and l3 + l2 > l1:
     if l1 == l2 and l1 == l3:
         print('equilatero')
     elif l1 == l2 or l1 == l3 or l2 == l3:
